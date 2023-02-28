33c256dac87a9a512a70ab27e0709475b1303627
Endless (Mateo L.) <mateo@endlessloop.me>
2023-02-28 19:11:38 -0300

    Add changes from dev branch (#1)
    * Specified which responses were ours and deleted PDF
    
    * Now removed PDF file correctly
    
    * Reordered TSV files and removed unnecessary files
    
    * Removed original analysis.R
    
    * Renamed analysis
    
    * Update README.md
    
    * Update README.md
    
    * More R cleaning up
    
    * Added pngs
    
    * moved original file
    
    * Added excel file

--- analysis.R
+++ analysis.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(lme4)
 library(sjPlot)
 
-gpt_responses_categorized_tsv <- "data/output/2022_10_02_13_26_48_results_categorized.tsv"
+gpt_responses_categorized_tsv <- "data/output/results_merged_old_new_categorized.tsv"
 gpt_responses_categorized <- read_tsv(gpt_responses_categorized_tsv)
 
 # delete prompt and response text columns, as they are not needed for the analysis
@@ -11,7 +11,7 @@ gpt_responses_categorized <- read_tsv(gpt_responses_categorized_tsv)
 gpt_responses_categorized <- gpt_responses_categorized %>% select(-prompt, -response, -logprobs, -gtp3_model)
 
 # most importantly "null" should be the first level, the reference.
-gpt_responses_categorized$modifier <- factor(gpt_responses_categorized$modifier, levels = c("null", "truthful", "friendly", "agreeable", "agreeable_b"))
+gpt_responses_categorized$modifier <- factor(gpt_responses_categorized$modifier, levels = c("null", "truthful", "friendly", "agreeable", "agreeable_b", "creative", "warm", "outgoing", "humorous", "intellectual", "skilful", "attentive", "withdrawn", "neutral", "ambitious", "outspoken", "frank", "straightforward", "skeptical", "upfront"))
 gpt_responses_categorized$question <- as.factor(gpt_responses_categorized$question)
 gpt_responses_categorized$question_wrapper <- as.factor(gpt_responses_categorized$question_wrapper)
 gpt_responses_categorized$interaction_type <- factor(gpt_responses_categorized$interaction_type, levels = c("zero_shot", "single_shot"))
@@ -26,35 +26,6 @@ gpt_responses_categorized$question_wrapper_type <- ifelse(grepl("That's so cool!
 
 gpt_responses_categorized$question_wrapper_type <- factor(gpt_responses_categorized$question_wrapper_type, levels = c("positive", "standard"))
 
-# now do regression analysis
-m1 <- glmer(response_type ~
-              modifier +
-              interaction_type +
-              question_wrapper_type +
-              (1 | question),
-              data = gpt_responses_categorized,
-              family = binomial(link = "logit"),
-              glmerControl(optimizer = "bobyqa",
-                optCtrl = list(maxfun = 100000)))
-print(summary(m1))
-plot(m1)
-plot_model(m1,
-            type = "std2",
-            title = "Both zero-shot and single-shot",
-            show.intercept = TRUE)
-# now do regression analysis only on single shot
-m2 <- glmer(response_type ~
-              modifier +
-              question_wrapper_type +
-              (1 | question),
-              data = gpt_responses_categorized[
-                gpt_responses_categorized$interaction_type == "single_shot", ],
-              family = binomial(link = "logit"),
-              glmerControl(optimizer = "bobyqa",
-                optCtrl = list(maxfun = 100000)))
-print(summary(m2))
-plot(m2)
-plot_model(m2, type = "std2", title = "Single-shot only", show.intercept = TRUE)
 
 # plot overall response type distribution
 gpt_responses_categorized %>%
@@ -69,18 +40,6 @@ gpt_responses_categorized %>%
 # plot response type distribution by modifier
 
 gpt_responses_categorized %>%
-  ggplot(aes(x = response_type, fill = modifier)) +
-  geom_bar() +
-  labs(
-    title = "Response type distribution by modifier",
-    x = "Response type",
-    y = "Count"
-  )
-
-
-# plot response type distribution by modifier
-
-gpt_responses_categorized %>%
   ggplot(aes(x = modifier, fill = response_type)) +
   geom_bar(position = "dodge") +
   labs(
@@ -88,6 +47,185 @@ gpt_responses_categorized %>%
     x = "Response type",
     y = "Count"
   )
+ggsave("data/output/by_modifier.png", width = 20, height = 20)
+
+
+# plot response type distribution by modifier (only single_shot)
+
+gpt_responses_categorized %>%
+  filter(interaction_type %in% c("single_shot")) %>%
+  ggplot(aes(x = modifier, fill = response_type)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Response type distribution by modifier",
+    x = "Response type",
+    y = "Count"
+  )
+ggsave("data/output/by_modifier_single_shot.png", width = 20, height = 20)
+
+
+# plot response type distribution by modifier (only some)
+
+gpt_responses_categorized %>%
+  filter(modifier %in% c("truthful", "creative", "skilful", "attentive")) %>%
+  ggplot(aes(x = modifier, fill = response_type)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Response type distribution for Truthful, vs Truthful + (Creative, Skilful, Attentive)",
+    x = "Response type",
+    y = "Count"
+  )
+ggsave("data/output/by_modifier_relevant.png", width = 8)
+
+
+# plot response type distribution by modifier (only some)
+
+gpt_responses_categorized %>%
+  filter(modifier %in% c("truthful", "agreeable", "attentive", "friendly")) %>%
+  ggplot(aes(x = modifier, fill = response_type)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Response type distribution for Truthful, vs Truthful + (Agreeable, Attentive, Friendly)",
+    x = "Response type",
+    y = "Count"
+  )
+ggsave("data/output/by_modifier_relevant2.png", width = 8)
+
+# plot response type distribution by modifier (only some)
+
+gpt_responses_categorized %>%
+  filter(modifier %in% c("truthful", "outgoing", "withdrawn")) %>%
+  ggplot(aes(x = modifier, fill = response_type)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Response type distribution for Truthful, vs Truthful + (Outgoing, Withdrawn)",
+    x = "Response type",
+    y = "Count"
+  )
+ggsave("data/output/by_modifier_relevant3.png", width = 8)
+
+
+
+# plot to compare response types for "truthful" and "agreeable" modifiers
+
+gpt_responses_categorized %>%
+  #filter(interaction_type %in% c("single_shot")) %>%  
+  filter(modifier %in% c("truthful", "agreeable")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + agreeable response type distribution",
+    x = "Modifier",
+    y = "Count"
+  )
+
+# plot to compare response types for "truthful" and "outgoing" modifiers
+
+gpt_responses_categorized %>%
+  #filter(interaction_type %in% c("single_shot")) %>%  
+  filter(modifier %in% c("truthful", "outgoing")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + outgoing response type distribution",
+    x = "Modifier",
+    y = "Count"
+  )
+
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_outgoing_response_type_distribution.png")
+
+
+# plot to compare response types for "truthful" and "straightforward" modifiers
+
+gpt_responses_categorized %>%
+  #filter(interaction_type %in% c("single_shot")) %>%  
+  filter(modifier %in% c("truthful", "straightforward")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + Straightforward response type distribution",
+    x = "Modifier",
+    y = "Count"
+  )
+
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_straightforward_response_type_distribution.png")
+
+
+# plot to compare response types for "truthful" and "attentive" modifiers
+
+gpt_responses_categorized %>%
+  #filter(interaction_type %in% c("single_shot")) %>%  
+  filter(modifier %in% c("truthful", "attentive")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + attentive response type distribution",
+    x = "Modifier",
+    y = "Count"
+  )
+
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_attentive_response_type_distribution.png")
+
+# plot to compare response types for "truthful" and "warm" modifiers
+
+gpt_responses_categorized %>%
+  #filter(interaction_type %in% c("single_shot")) %>%  
+  filter(modifier %in% c("truthful", "warm")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + warm response type distribution",
+    x = "Modifier",
+    y = "Count"
+  )
+
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_warm_response_type_distribution.png")
+
+
+
+# plot to compare response types for "truthful" and "skeptical" modifiers
+
+gpt_responses_categorized %>%
+  #filter(interaction_type %in% c("single_shot")) %>%  
+  filter(modifier %in% c("truthful", "skeptical")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + skeptical response type distribution",
+    x = "Modifier",
+    y = "Count"
+  )
+
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_skeptical_response_type_distribution.png")
+
+
+# plot to compare response types for "truthful" and "creative" modifiers
+
+gpt_responses_categorized %>%
+  #filter(interaction_type %in% c("single_shot")) %>%  
+  filter(modifier %in% c("truthful", "creative")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + creative response type distribution",
+    x = "Modifier",
+    y = "Count"
+  )
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_creative_response_type_distribution.png")
+
+
 
 # plot to compare response types for "null" and "truthful" modifiers
 
@@ -177,6 +315,22 @@ gpt_responses_categorized %>%
 # save plot as png
 ggsave("data/output/truthful_vs_truthful_agreeable_response_type_distribution_by_interaction_type.png")
 
+# plot to compare response types for "truthful" and "skeptical" modifiers
+
+gpt_responses_categorized %>%
+  filter(modifier %in% c("truthful", "skeptical")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + skeptical response type distribution",
+    x = "Modifier",
+    y = "Count"
+  ) +
+  facet_wrap(~interaction_type)
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_skeptical_response_type_distribution_by_interaction_type.png")
+
 
 # plot to compare truthful and agreeable modifiers by question_wrapper_type
 gpt_responses_categorized %>%
@@ -191,4 +345,21 @@ gpt_responses_categorized %>%
   facet_wrap(~question_wrapper_type)
 
 # save plot as png
-ggsave("data/output/truthful_vs_agreeable_response_type_distribution_by_question_wrapper_type.png")
+ggsave("data/output/truthful_vs_truthful_agreeable_response_type_distribution_by_question_wrapper_type.png")
+
+
+
+# plot to compare truthful and outgoing modifiers by question_wrapper_type
+gpt_responses_categorized %>%
+  filter(modifier %in% c("truthful", "outgoing")) %>%
+  ggplot(aes(x = response_type, fill = modifier)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "Truthful vs Truthful + outgoing response type distribution by question wrapper type",
+    x = "Response type",
+    y = "Count"
+  ) +
+  facet_wrap(~question_wrapper_type)
+
+# save plot as png
+ggsave("data/output/truthful_vs_truthful_outgoing_response_type_distribution_by_question_wrapper_type.png")
